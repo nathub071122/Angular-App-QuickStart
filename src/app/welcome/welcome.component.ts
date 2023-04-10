2653166dff3cf2b0d2a34dd081f8a83814519fe1
nathub071122 <117662195+nathub071122@users.noreply.github.com>
2023-04-10 13:55:14 +0530

    1 file modified

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector:'welcome',
-  templateUrl:'welcome-component.html',
-  styleUrl:'welcome.component.css'
+  templateUrl:'welcome.component.html',
+  styleUrls:['welcome.component.css']
 })
 export class WelcomeComponent {
 
